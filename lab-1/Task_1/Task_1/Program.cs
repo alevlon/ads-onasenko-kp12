fc2da5045379fb5341ea747714e41ccce1315fed
alevlon <44722037+alevlon@users.noreply.github.com>
2021-10-19 00:41:41 +0300

    laboratory work completed
    Task_1 and Task_2 contain solutions

---
+++ lab-1/Task_1/Task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+
+            Write("x = ");
+            x = double.Parse(ReadLine());
+
+            Write("y = ");
+            y = double.Parse(ReadLine());
+
+            Write("z = ");
+            z = double.Parse(ReadLine());
+
+
+            if (z == 0)
+            {
+                WriteLine("Data is incorrect");
+            }
+            else 
+            {
+                double a = Cos(x + x * y / z);
+                double cos_a = Cos(a);
+                if (cos_a == 0)
+                {
+                    WriteLine("Data is incorrect");
+                }
+                else 
+                {
+                    double b = Pow(x, 3) / cos_a;
+
+                    WriteLine($"a = {a}");
+                    WriteLine($"b = {b}");
+                }
+            }
+        }
+    }
+}
---
+++ lab-1/Task_2/Task_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d, m;
+            
+            Console.Write("Enter d: ");
+            d = int.Parse(ReadLine());
+            Console.Write("Enter m: ");
+            m = int.Parse(ReadLine());
+
+            int[] days_in_month = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            string[] day_of_week = new string[7] { "Понедiлок", "Вiвторок", "Середа", "Четвер", "П’ятниця", "Субота ", "Недiля" };
+
+
+            if (m < 1 || m > 12) 
+            {
+                Write("Data is incorrect");
+                return;
+            }
+            if (d < 1 || d > days_in_month[m - 1]) 
+            {
+                Write("Data is incorrect");
+                return;
+            }
+
+            int month_current = 0;
+            int day_current = 1;
+            int day_of_week_current = 2;
+
+            while ((day_current != d) || ((month_current + 1) != m)) 
+            {
+                day_current++;
+                if (day_current > days_in_month[month_current]) 
+                {
+                    day_current = 1;
+                    month_current++;
+                }
+                day_of_week_current++;
+                if (day_of_week_current > 6) day_of_week_current = 0;
+            }
+
+            if (d < 10 && m < 10) WriteLine($"0{d}/0{m} - це {day_of_week[day_of_week_current]}");
+            else if (d < 10 && m > 10) WriteLine($"0{d}/{m} - це {day_of_week[day_of_week_current]}");
+            else if (d > 10 && m < 10) WriteLine($"{d}/0{m} - це {day_of_week[day_of_week_current]}");
+            else WriteLine($"{d}/{m} - це {day_of_week[day_of_week_current]}");
+        }
+    }
+}
