6eb70cf1ee8d95993a95cd152f2296843a5aa366
alevlon <44722037+alevlon@users.noreply.github.com>
2021-12-06 21:20:10 +0200

    Writing program code

---
+++ lab-3/Task_5/Task_5/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int i = 0, j = 0;
+
+            try
+            {
+                Write("Enter n: "); n = int.Parse(ReadLine());
+                if (n < 0) throw new Exception("Input string was not in a correct format.");
+
+            }
+            catch (Exception e) 
+            {
+                WriteLine($"{e.Message}");
+                return;
+            }
+
+            int[] arr = new int[n];
+            Random rdn = new Random();
+            for (i = 0; i < n; i++) arr[i] = rdn.Next(-1000, 100);
+
+
+            List<int> mass_for_sort = new List<int>();
+            for (i = 0; i < n; i++) 
+            {
+                if (arr[i] < 0 && arr[i]%2 == 0) mass_for_sort.Add(Abs(arr[i]));
+            }
+
+
+            int d = mass_for_sort.Count / 2;
+            while (d >= 1)
+            {
+                for (i = d; i < mass_for_sort.Count; i++)
+                {
+                    int current = mass_for_sort[i];
+                    j = i;
+                    while ((j >= d) && (mass_for_sort[j - d] > current)) 
+                    {
+                        int t = mass_for_sort[j];
+                        mass_for_sort[j] = mass_for_sort[j - d];
+                        mass_for_sort[j - d] = t;
+
+                        j = j - d;
+                    }   
+                }
+                
+                d = d / 2;
+            }
+            for (i = 0; i < mass_for_sort.Count; i++) mass_for_sort[i] *= -1;
+
+
+
+            WriteLine("=================== The unsorted array is  ===================");
+            for (i = 0; i < n; i++) 
+            {
+                if (arr[i] < 0 && arr[i] % 2 == 0)
+                {
+                    Write(" " + arr[i] + " ");
+                }
+                else 
+                {
+                    Write(" ");
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Write(" " + arr[i] + " ");
+                    Console.ResetColor();
+                    Write(" ");
+                }
+            }
+            WriteLine();
+
+            int x = 0;
+            WriteLine("=================== The sorted array is  ===================");
+            for (i = 0; i < n; i++)
+            {
+                if (arr[i] < 0 && arr[i] % 2 == 0)
+                {
+                    Write(" " + mass_for_sort[x] + " ");
+                    x++;
+                }
+                else
+                {
+                    Write(" ");
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Write(" " + arr[i] + " ");
+                    Console.ResetColor();
+                    Write(" ");
+                }
+            }
+            WriteLine();
+
+
+
+        }
+    }
+}
